786bf0b232bfab67464251f270f6a900f331e333
Lucas Levit <lucassebas96@gmail.com>
2022-09-23 02:27:20 -0300

    subida de la nav

---
+++ src/componentes/CartWidget.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import cart from "../assets/img/cart.svg"
+
+export const CartWidget = () => {
+    return (
+        <div><img className= "logo" src= {cart} alt="carrito"></img></div>
+    )
+    
+
+}
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,30 @@
+import { React } from "react";
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => {
+    return (<nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Bazar Lucas</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Tienda</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Contacto</a>
+                    </li>
+                </ul>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-primary" type="submit">Buscar</button>
+                </form>
+            </div>
+        </div>
+        <div classNameName="carrito"> <CartWidget /></div>
+    </nav>
+
+    )
+}
